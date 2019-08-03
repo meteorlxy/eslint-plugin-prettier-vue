546d098c8cdefbed1dfe0c06b27deb7fc3acde3c
meteorlxy <meteor.lxy@foxmail.com>
2019-08-03 18:10:00 +0800

    feat: process all sfc blocks in the same way

---
+++ test/non-script.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input id="multiple" class="attributes" v-model="attrOrder">
+  </div>
+</template>
+
+<style></style>
+
+<style>
+div { color: green }
+</style>
+
+<style lang="scss">
+$text-color: green;
+div { color: $text-color; }
+</style>
